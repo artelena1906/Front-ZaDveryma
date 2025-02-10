19707d50c73c8a93f9baf64ec486d848e90c3c9d
lena <ae.petrovna@gmail.com>
2025-02-10 11:27:40 +0200

    Исправлен адаптив блока фото

--- src/app/MainPage/MainPagePhoto.tsx
+++ src/app/MainPage/MainPagePhoto.tsx
@@ -14,7 +14,6 @@ interface Photo {
 export default function Photo() {
   const [photos, setPhotos] = useState<Photo[]>([]);
 
-
   useEffect(() => {
     fetch("/MainPageHeader.json")
       .then((res) => res.json())
@@ -27,51 +26,56 @@ export default function Photo() {
 
   return (
     <>
-      <div className={styles.carousel}>
-        {/* <h2 className={styles.h2}>Фото з наших подорожей</h2> */}
-        <Swiper
-          modules={[Navigation]}
-          navigation
-          // slidesPerView={4}
-          // slidesOffsetBefore={15} // Отступ слева
-          pagination={{ clickable: true }}
-          breakpoints={{
-            200: {
-              slidesPerView: 2,
-              spaceBetween: 5, // Уменьшаем отступ для маленьких экранов
-            },
-            520: {
-              slidesPerView: 2,
-              spaceBetween: 5, // Отступ для экранов средней ширины
-            },
-            800: {
-              slidesPerView: 3,
-              spaceBetween: 10, // Отступ для планшетов
-            },
-            1050: {
-              slidesPerView: 4,
-              spaceBetween: 10, // Отступ для небольших десктопов
-            },
-            1350: {
-              slidesPerView: 5,
-              spaceBetween: 15, // Отступ для широких экранов
-            },
-          }}
-          className={styles.carouselTrack}
-        >
-          {photos.map((photo, index) => (
-            <SwiperSlide key={index} className={styles.carouselTrack}>
-              <Image
-                src={photo.img}
-                alt={photo.alt}
-                className={styles.image}
-                loading="lazy"
-                width={260}
-                height={150}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+      <div className={styles.container}>
+        <h2 className={styles.h2}>Фотогалерея</h2>
+        <div className={styles.carouselcontainer}>
+          <Swiper
+            modules={[Navigation]}
+            navigation
+            // slidesPerView={4}
+            // slidesOffsetBefore={15} // Отступ слева
+            pagination={{ clickable: true }}
+            breakpoints={{
+              420: {
+                slidesPerView: 1,
+                // spaceBetween: 5, // Уменьшаем отступ для маленьких экранов
+              },
+              421: {
+                slidesPerView: 2,
+                spaceBetween: 5, // Отступ для экранов средней ширины
+              },
+              800: {
+                slidesPerView: 3,
+                spaceBetween: 10, // Отступ для планшетов
+              },
+              1050: {
+                slidesPerView: 4,
+                // spaceBetween: 10, // Отступ для небольших десктопов
+              },
+              1350: {
+                slidesPerView: 5,
+                // spaceBetween: 15, // Отступ для широких экранов
+              },
+              1500: {
+                slidesPerView: 6,
+                // spaceBetween: 15, // Отступ для широких экранов
+              },
+            }}
+          >
+            {photos.map((photo, index) => (
+              <SwiperSlide key={index} className={styles.carouselTrack}>
+                <Image
+                  src={photo.img}
+                  alt={photo.alt}
+                  className={styles.image}
+                  loading="lazy"
+                  width={260}
+                  height={150}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
       <hr className={styles.h}></hr>
     </>
