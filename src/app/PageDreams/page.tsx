3a5318cebdd141b9d86f8aac30bad5a8694b7ee1
lena <ae.petrovna@gmail.com>
2025-04-02 15:38:49 +0300

    доработаны карточки на странице Мрії

--- src/app/PageDreams/page.tsx
+++ src/app/PageDreams/page.tsx
@@ -1,9 +1,108 @@
+// "use client";
+// import { useState, useEffect } from "react";
+// import Image from "next/image";
+// import styles from "./css/PageDreams.module.css";
+// import WithUs from "../MainPage/MainPageBodyWithUs";
+// import BookingForm from "../../components/PageBookingForm"; // Импортируем BookingForm
+
+// export default function PageDreams() {
+//   interface CountryItem {
+//     id: number;
+//     namecountry: string;
+//     descriptioncountry: string;
+//     urlphoto: string;
+//   }
+
+//   interface DreamsItem {
+//     title: string;
+//     description: string;
+//     country: CountryItem[];
+//   }
+
+//   const [dreams, setDreams] = useState<DreamsItem | null>(null);
+//   const [showForm, setShowForm] = useState(false); // Состояние для показа формы
+//   const [selectedCountry, setSelectedCountry] = useState<string | null>(null); // Выбранная страна
+
+//   useEffect(() => {
+//     fetch("/PageDreams.json")
+//       .then((res) => {
+//         if (!res.ok) {
+//           throw new Error("Не удалось загрузить JSON");
+//         }
+//         return res.json();
+//       })
+//       .then((data) => {
+//         console.log("Загруженные данные:", data);
+//         setDreams(data.bodyData);
+//       })
+//       .catch((error) => {
+//         console.error("Ошибка загрузки данных:", error);
+//       });
+//   }, []);
+
+//   const handleJoinClick = (countryName: string) => {
+//     setSelectedCountry(countryName); // Устанавливаем выбранную страну
+//     setShowForm(true); // Показываем форму
+//   };
+
+//   const handleCloseForm = () => {
+//     setShowForm(false); // Скрываем форму
+//     setSelectedCountry(null); // Очищаем выбранную страну
+//   };
+
+//   if (!dreams) {
+//     return <div>Завантаження...</div>;
+//   }
+
+//   return (
+//     <div className={styles.containerDreams}>
+//       <h3>{dreams.title}</h3>
+//       <p>{dreams.description}</p>
+//       <div className={styles.container}>
+//         {dreams.country && dreams.country.length > 0 ? (
+//           dreams.country.map((country, index) => (
+//             <div key={index} className={styles.containerCountry}>
+//               <h4>{country.namecountry}</h4>
+//               <Image
+//                 src={country.urlphoto}
+//                 alt={country.namecountry}
+//                 width={300}
+//                 height={200}
+//               />
+//               <p>{country.descriptioncountry}</p>
+//               <div className={styles.Button}>
+//                 <button
+//                   className={styles.interestedButton}
+//                   onClick={() => handleJoinClick(country.namecountry)} // Открываем форму
+//                 >
+//                   Долучитися до мрії!
+//                 </button>
+//               </div>
+//             </div>
+//           ))
+//         ) : (
+//           <p>Країни відсутні</p>
+//         )}
+//       </div>
+//       <div>
+//         <WithUs />
+//       </div>
+
+//       {/* Показываем форму, если showForm === true */}
+//       {showForm && selectedCountry && (
+//         <BookingForm tourTitle={selectedCountry} onClose={handleCloseForm} />
+//       )}
+//     </div>
+//   );
+// }
+
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "./css/PageDreams.module.css";
 import WithUs from "../MainPage/MainPageBodyWithUs";
-import BookingForm from "../../components/PageBookingForm"; // Импортируем BookingForm
+import BookingForm from "../../components/PageBookingForm";
+import Link from "next/link";
 
 export default function PageDreams() {
   interface CountryItem {
@@ -11,6 +110,10 @@ export default function PageDreams() {
     namecountry: string;
     descriptioncountry: string;
     urlphoto: string;
+    month: string;
+    days: string;
+    count: string;
+    city: string;
   }
 
   interface DreamsItem {
@@ -20,34 +123,30 @@ export default function PageDreams() {
   }
 
   const [dreams, setDreams] = useState<DreamsItem | null>(null);
-  const [showForm, setShowForm] = useState(false); // Состояние для показа формы
-  const [selectedCountry, setSelectedCountry] = useState<string | null>(null); // Выбранная страна
+  const [showForm, setShowForm] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
 
   useEffect(() => {
     fetch("/PageDreams.json")
       .then((res) => {
-        if (!res.ok) {
-          throw new Error("Не удалось загрузить JSON");
-        }
+        if (!res.ok) throw new Error("Не удалось загрузить JSON");
         return res.json();
       })
       .then((data) => {
         console.log("Загруженные данные:", data);
         setDreams(data.bodyData);
       })
-      .catch((error) => {
-        console.error("Ошибка загрузки данных:", error);
-      });
+      .catch((error) => console.error("Ошибка загрузки данных:", error));
   }, []);
 
   const handleJoinClick = (countryName: string) => {
-    setSelectedCountry(countryName); // Устанавливаем выбранную страну
-    setShowForm(true); // Показываем форму
+    setSelectedCountry(countryName);
+    setShowForm(true);
   };
 
   const handleCloseForm = () => {
-    setShowForm(false); // Скрываем форму
-    setSelectedCountry(null); // Очищаем выбранную страну
+    setShowForm(false);
+    setSelectedCountry(null);
   };
 
   if (!dreams) {
@@ -57,26 +156,37 @@ export default function PageDreams() {
   return (
     <div className={styles.containerDreams}>
       <h3>{dreams.title}</h3>
-      <p>{dreams.description}</p>
+      <p className={styles.containerDreamsP}>{dreams.description}</p>
       <div className={styles.container}>
         {dreams.country && dreams.country.length > 0 ? (
           dreams.country.map((country, index) => (
-            <div key={index} className={styles.containerCountry}>
-              <h4>{country.namecountry}</h4>
-              <Image
-                src={country.urlphoto}
-                alt={country.namecountry}
-                width={300}
-                height={200}
-              />
-              <p>{country.descriptioncountry}</p>
-              <div className={styles.Button}>
-                <button
-                  className={styles.interestedButton}
-                  onClick={() => handleJoinClick(country.namecountry)} // Открываем форму
-                >
-                  Долучитися до мрії!
-                </button>
+            <div key={index} className={styles.cardContainer}>
+              <div className={styles.card}>
+                {/* Лицевая сторона */}
+                <div className={styles.cardFront}>
+                  <Image
+                    src={country.urlphoto}
+                    alt={country.namecountry}
+                    width={300}
+                    height={450}
+                    style={{ objectFit: 'cover' }}
+                  />
+                  <div className={styles.cardFrontContent}>
+                    <h4>{country.namecountry}</h4>
+                  </div>
+                </div>
+                {/* Обратная сторона */}
+                <div className={styles.cardBack}>
+                  <h4>{country.namecountry}</h4>
+                  <p className={styles.cardBackMonth}>{country.month}</p>
+                  <p className={styles.cardBackP}><strong>Тривалість:</strong> {country.days}</p>
+                  <p className={styles.cardBackP}><strong>Кількість:</strong> {country.count}</p>
+                    <p className={styles.descriptioncountry}>{country.descriptioncountry}</p>
+                  <p className={styles.cardBackCity}><strong>Міста:</strong> {country.city}</p>
+                  <Link href={`/tours/${country.namecountry.toLowerCase()}`} className={styles.detailsButton}>
+                    Детальніше про тур
+                  </Link>
+                </div>
               </div>
             </div>
           ))
@@ -87,11 +197,9 @@ export default function PageDreams() {
       <div>
         <WithUs />
       </div>
-
-      {/* Показываем форму, если showForm === true */}
       {showForm && selectedCountry && (
         <BookingForm tourTitle={selectedCountry} onClose={handleCloseForm} />
       )}
     </div>
-  );
+);
 }
